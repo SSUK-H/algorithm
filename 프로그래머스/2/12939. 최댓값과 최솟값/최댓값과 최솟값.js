b895bb69fb966eb2a6a8ea4c7379ffc506083671
홍성숙 <134491629+SSUK-H@users.noreply.github.com>
2024-07-23 21:21:42 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    const number = s.split(" ")
+    const min = Math.min(...number)
+    const max = Math.max(...number)
+    var answer = `${min} ${max}`;
+    
+    // return answer;
+}
